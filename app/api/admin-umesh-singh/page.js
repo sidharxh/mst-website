78a6d1450ff230137c9fa0401ba04767a4bb0a67
sidharth singh <sid.cse19@gmail.com>
2026-03-09 14:03:27 +0530

    added tracking data endpoint called 'admin-umesh-singh'

---
+++ app/api/admin-umesh-singh/page.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function AdminPage() {
+  redirect('https://docs.google.com/spreadsheets/d/1mXnBFf-ncM9MpGVUWPvggfzNlFPZzoa7jOqHlNUxyFY/edit');
+}
